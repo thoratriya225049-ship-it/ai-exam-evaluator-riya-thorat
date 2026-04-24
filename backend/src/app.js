0d3700a48a864b62b8e4d138d229aa3d57529a21
riya <thoratriya225049@gmail.com>
2026-04-24 20:34:09 +0530

    Day 0:

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
